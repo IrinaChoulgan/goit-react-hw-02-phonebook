39a260aa44995a76d501de6bff0e859041cdbf14
IrinaChoulgan <irina_choulgan@icloud.com>
2021-10-08 22:41:22 +0300

    +

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -45,7 +45,7 @@ export class ContactForm extends Component {
           type="text"
           name="name"
           value={name}
-          placeholder="enter your name"
+          placeholder="enter name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
           required
@@ -58,7 +58,7 @@ export class ContactForm extends Component {
           type="tel"
           name="number"
           value={number}
-          placeholder="enter your phone number"
+          placeholder="enter phone number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
           required
